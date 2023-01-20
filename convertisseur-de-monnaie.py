24cca491b113a40c1839b71ae20b181ee6a880c8
antony-lucide <antony.lucide@laplateforme.io>
2023-01-20 22:53:39 +0100

    Ceci est le rendu du projet convertisseur de monnaie, inachevé

--- convertisseur-de-monnaie.py
+++ convertisseur-de-monnaie.py
@@ -21,8 +21,17 @@ def transition(): #Fonction permettant de convertir
     dict = OPTION.get(résultat,None)#Récuperation du dictionnaire
     converti = float(dict)*(somme)#Calcule
     résult1.insert(INSERT, "le prix en", INSERT, résultat,INSERT," = ",converti)#Réponse
+    if equation !="":
+            try:
+                result = eval(converti) #évalue l'équation
+            except:
+                result = "error"
+                equation = ""
+            résult1.config(text=converti)
+
 
     
+        
 
 
 gui = tk.Tk() #créer une nouvelle fenêtre sur linux ou window/mac
@@ -32,13 +41,13 @@ gui.geometry("600x400")
 résult1 = LabelFrame(gui, text="Taux",borderwidth=80, relief=SUNKEN, width=60, height=60)
 résult1.pack( fill=X, side=TOP, expand=True)
 equation = StringVar()
-résult2 = Entry(gui, textvariable=equation,width=60).place(x=50, y=150) 
+résult2 = Entry(gui, textvariable=equation,width=60).place(x=50, y=100) 
 résult3 = LabelFrame(gui, text="somme",borderwidth=80, relief=SUNKEN, width=70, height=70)
 résultat = StringVar
 résult3.pack(fill=X, side=BOTTOM, expand=True)
 result3 = Entry(gui, textvariable=résultat,width=70)
-Button(gui, text="calcule", command=transition(),width=20).place(x=9, y=180)
-result3 = Entry(gui, textvariable=résultat,width=70).place(x=40,y=450)
+Button(gui, text="calcule", command=transition,width=20).place(x=9, y=130)
+result3 = Entry(gui, textvariable=résultat,width=60).place(x=40,y=300)
 résultat = LabelFrame(gui, text='',borderwidth=80, relief=SUNKEN, width=70, height=70).place(x=9, y=500)
 
 gui.mainloop()
